b0728f9f7e47372a028767ce24cb8daee29efe96
IOT Instructor <brashap@cnm.edu>
2020-04-14 17:24:04 -0600

    added pump

--- PlantWater/src/PlantWater.cpp
+++ PlantWater/src/PlantWater.cpp
@@ -12,15 +12,20 @@
 #include <Particle.h>
 #include <JsonParserGeneratorRK.h>
 
-
 void setup();
 void loop();
+bool waterPlant(int moistVal);
 void printMoist(int moistVal);
-void createEventPayLoad(int moistValue, int tempValue);
-#line 11 "c:/Users/IoTPa/Documents/jarvis/PlantWater/src/PlantWater.ino"
+void createEventPayLoad(int moistValue, int tempValue, bool waterED);
+#line 10 "c:/Users/IoTPa/Documents/jarvis/PlantWater/src/PlantWater.ino"
 int soilPin = A2;
 int moist;
-int soilDelay = 10000;
+int soilDelay = 60000;
+
+int threshold = 3500;
+int pumpPin = D4;
+int waterTime = 2500;
+bool watered;
 
 int tempPin = A3;
 int temp;
@@ -33,21 +38,42 @@ String t2;
 void setup() {
   Serial.begin(9600);
   pinMode(soilPin,INPUT);
+  pinMode(pumpPin,OUTPUT);
   Time.zone(-6); // set timezone to MDT
   Particle.syncTime(); 
   Particle.variable("Moisture", moist);
   Particle.variable("Temperature", temp);
+  Particle.variable("Plant Watered", watered);
 }
 
 void loop() {
   moist = analogRead(soilPin);
+  watered = waterPlant(moist);
+
   temp = analogRead(tempPin);
   printMoist(moist);
   Particle.publish("Moisture", String(moist),PRIVATE);
   Particle.publish("Temperature", String(temp),PRIVATE);
-  createEventPayLoad(moist,temp);
+  Particle.publish("Plant Watered", String(watered),PRIVATE);
+  createEventPayLoad(moist,temp,watered);
+
   delay(soilDelay);
 }
+
+bool waterPlant(int moistVal) {
+  if(moistVal > threshold) {
+    digitalWrite(pumpPin,HIGH);
+    delay(waterTime);
+    digitalWrite(pumpPin,LOW);
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+    
+}
+
 
 void printMoist(int moistVal) {
   t = Time.timeStr(); 
@@ -60,13 +86,14 @@ void printMoist(int moistVal) {
   Serial.printf("The time is %s \n",current);
 }
 
-void createEventPayLoad(int moistValue, int tempValue) {
+void createEventPayLoad(int moistValue, int tempValue, bool waterED) {
   JsonWriterStatic<256> jw;
   {
     JsonWriterAutoObject obj(&jw);
 
     jw.insertKeyValue("Moisture", moistValue);
     jw.insertKeyValue("Temperature", tempValue);
+    jw.insertKeyValue("Plant Watered", waterED);
   }
   Particle.publish("env-vals",jw.getBuffer(), PRIVATE);
 }
--- PlantWater/src/PlantWater.ino
+++ PlantWater/src/PlantWater.ino
@@ -7,10 +7,14 @@
 #include <Particle.h>
 #include <JsonParserGeneratorRK.h>
 
-
 int soilPin = A2;
 int moist;
-int soilDelay = 10000;
+int soilDelay = 60000;
+
+int threshold = 3500;
+int pumpPin = D4;
+int waterTime = 2500;
+bool watered;
 
 int tempPin = A3;
 int temp;
@@ -23,21 +27,42 @@ String t2;
 void setup() {
   Serial.begin(9600);
   pinMode(soilPin,INPUT);
+  pinMode(pumpPin,OUTPUT);
   Time.zone(-6); // set timezone to MDT
   Particle.syncTime(); 
   Particle.variable("Moisture", moist);
   Particle.variable("Temperature", temp);
+  Particle.variable("Plant Watered", watered);
 }
 
 void loop() {
   moist = analogRead(soilPin);
+  watered = waterPlant(moist);
+
   temp = analogRead(tempPin);
   printMoist(moist);
   Particle.publish("Moisture", String(moist),PRIVATE);
   Particle.publish("Temperature", String(temp),PRIVATE);
-  createEventPayLoad(moist,temp);
+  Particle.publish("Plant Watered", String(watered),PRIVATE);
+  createEventPayLoad(moist,temp,watered);
+
   delay(soilDelay);
 }
+
+bool waterPlant(int moistVal) {
+  if(moistVal > threshold) {
+    digitalWrite(pumpPin,HIGH);
+    delay(waterTime);
+    digitalWrite(pumpPin,LOW);
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+    
+}
+
 
 void printMoist(int moistVal) {
   t = Time.timeStr(); 
@@ -50,13 +75,14 @@ void printMoist(int moistVal) {
   Serial.printf("The time is %s \n",current);
 }
 
-void createEventPayLoad(int moistValue, int tempValue) {
+void createEventPayLoad(int moistValue, int tempValue, bool waterED) {
   JsonWriterStatic<256> jw;
   {
     JsonWriterAutoObject obj(&jw);
 
     jw.insertKeyValue("Moisture", moistValue);
     jw.insertKeyValue("Temperature", tempValue);
+    jw.insertKeyValue("Plant Watered", waterED);
   }
   Particle.publish("env-vals",jw.getBuffer(), PRIVATE);
 }
