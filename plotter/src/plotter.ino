8970e99bd830f9dd09cf966e4b1bb1c72970c346
IOT Instructor <brashap@cnm.edu>
2020-11-29 09:14:07 -0700

    jarvis

---
+++ plotter/src/plotter.ino
@@ -0,0 +1,61 @@
+/*
+ * Project plotter
+ * Description:
+ * Author:
+ * Date:
+ */
+#include "math.h"
+
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1306.h"
+
+#define OLED_RESET D4
+Adafruit_SSD1306 display(OLED_RESET);
+
+const int yMax = 100;
+const int yMin = 0;
+
+const float freq = .1;
+const int amp = 40;
+const int offset = 50;
+const unsigned int plotPeriod = 5; //x-axis steps in ms
+float data;
+unsigned int currentTime;
+unsigned int lastTime;
+
+void setup() {
+  Serial.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); 
+  display.setRotation(2);
+  display.display(); // show splashscreen
+  delay(2000);
+  display.clearDisplay();   // clears the screen and buffer
+}
+
+
+void loop() {
+  currentTime = millis();
+  if((currentTime-lastTime)>plotPeriod) {
+    data = amp*sin(2*M_PI*freq*(currentTime/1000.0))+offset;
+    oledPlot(data);
+    lastTime = currentTime;
+    Serial.printf("Time = %f, Data = %i \n",currentTime/1000.0,data);
+  }
+}
+
+void oledPlot(int y) {
+  static int x=0;
+
+  y = map(y,yMin,yMax,63,0);
+  display.drawPixel(x, y, WHITE);
+  display.display();
+  x++;
+  if(x>127) {
+    x=0;
+    display.clearDisplay();
+  }
+}
+
+int mapFloat(float fromY, float fromMin, float fromMax, float toMin, float toMax) {
+  return (fromY - fromMin) * (toMax - toMin) / (fromMax - fromMin) + fromMin;
+}
